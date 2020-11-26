3e32f1709cbf71fcbe4b367ec724fb6ccf0f9b33
Chad Green <chadgreen@chadgreen.com>
2020-11-26 02:01:43 -0500

    Exploring as part of TaleLearnCode stream

---
+++ src/TwitchBot/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace TwitchBot
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//Bot bot = new Bot();
+			//bot.Connect(false);
+			//Console.ReadLine();
+			//bot.Discconect();
+
+			LiveMonitor liveMonitor = new LiveMonitor();
+			Console.ReadLine();
+			liveMonitor.StopMonitoring();
+
+		}
+	}
+}
---
+++ src/TwitchBot/TSHelper.cs
@@ -0,0 +1,39 @@
+﻿using Azure.Data.Tables;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwitchBot
+{
+	public static class TSHelper
+	{
+
+		public static void WriteData()
+		{
+
+			// Construct a new <see cref="TableClient" /> using a <see cref="TableSharedKeyCredential" />.
+			var tableClient = new TableClient(
+					new Uri(Settings.StorageUri),
+					"StreamSnapshot",
+					new TableSharedKeyCredential(Settings.StorageAccountName, Settings.StroageAccountKey));
+
+			// Create the table in the service.
+			tableClient.Create();
+
+			// Make a dictionary entity by defining a <see cref="TableEntity">.
+
+			//var entity = new TableEntity(partitionKey, rowKey)
+			//{
+			//	{ "Product", "Marker Set" },
+			//	{ "Price", 5.00 },
+			//	{ "Quantity", 21 }
+			//};
+
+			//Console.WriteLine($"{entity.RowKey}: {entity["Product"]} costs ${entity.GetDouble("Price")}.");
+		}
+
+
+	}
+}

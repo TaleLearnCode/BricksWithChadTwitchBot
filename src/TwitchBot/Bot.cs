2496b61d3b76d014f600accde9e17ed6100aa66c
Chad Green <chadgreen@chadgreen.com>
2020-12-01 01:22:27 -0500

    Added ResetProject command

--- src/TwitchBot/Bot.cs
+++ src/TwitchBot/Bot.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using TwitchLib.Client;
 using TwitchLib.Client.Events;
 using TwitchLib.Client.Models;
@@ -10,13 +11,17 @@ namespace TwitchBot
 	public class Bot
 	{
 
+		// TODO: Adding console logging
+		// TODO: Add a RestProject stream message
+
 		// TwitchTokenGenerator.com
 		ConnectionCredentials credentials = new ConnectionCredentials(Settings.ChannelName, Settings.AccessToken);
 		TwitchClient twitchClient = new TwitchClient();
 
-		int _BricksDroped = 0;
-		int _Oofs = 0;
+		private int _BricksDropped = 0;
+		private int _Oofs = 0;
 		private string _StreamKey;
+		private ProjectTracking _ProjectTracking = new();
 
 		internal void Connect(bool logEvents)
 		{
@@ -65,7 +70,7 @@ namespace TwitchBot
 			if (mostRecentStreamKey != _StreamKey)
 			{
 				_StreamKey = mostRecentStreamKey;
-				_BricksDroped = 0;
+				_BricksDropped = 0;
 				_Oofs = 0;
 			}
 
@@ -80,7 +85,12 @@ namespace TwitchBot
 				case "stats":
 					Stats();
 					break;
+				case "resetproject":
+					ResetProject(e);
+					break;
 			}
+
+			_ProjectTracking.Save();
 
 		}
 
@@ -89,16 +99,15 @@ namespace TwitchBot
 
 			if (commandArgs.Command.ChatMessage.IsBroadcaster || commandArgs.Command.ChatMessage.IsModerator)
 			{
-				if (commandArgs.Command.ArgumentsAsList.Count > 0)
-				{
-					if (int.TryParse(commandArgs.Command.ArgumentsAsList[0], out var numberOfBricksDropped))
-						_BricksDroped += numberOfBricksDropped;
-				}
-				else
-					_BricksDroped++;
+				int numberOfBricksDropped = 1;
+				if (commandArgs.Command.ArgumentsAsList.Any())
+					int.TryParse(commandArgs.Command.ArgumentsAsList[0], out numberOfBricksDropped);
+
+				_BricksDropped += numberOfBricksDropped;
+				UpdateProjectStats(bricksDropped: numberOfBricksDropped);
 			}
 
-			twitchClient.SendMessage(Settings.ChannelName, $"Chad has dropped {_BricksDroped} {(_BricksDroped == 1 ? "brick" : "bricks")} so far this stream.");
+			twitchClient.SendMessage(Settings.ChannelName, $"Chad has dropped {_BricksDropped} {(_BricksDropped == 1 ? "brick" : "bricks")} so far this stream.{(!string.IsNullOrWhiteSpace(_ProjectTracking.RowKey) ? $" During the {_ProjectTracking.RowKey} build, Chad has dropped {_ProjectTracking.BricksDropped} {(_ProjectTracking.BricksDropped == 1 ? "brick" : "bricks")}." : string.Empty)}");
 
 		}
 
@@ -106,17 +115,40 @@ namespace TwitchBot
 		{
 
 			if (commandArgs.Command.ChatMessage.IsBroadcaster || commandArgs.Command.ChatMessage.IsModerator)
+			{
 				_Oofs++;
+				UpdateProjectStats(oofs: 1);
+			}
 
-			twitchClient.SendMessage(Settings.ChannelName, $"Chad has had {_Oofs} {(_Oofs == 1 ? "oof" : "oofs")} so far this stream.");
-
+			twitchClient.SendMessage(Settings.ChannelName, $"Chad has dropped {_Oofs} {(_Oofs == 1 ? "oof" : "oofs")} so far this stream.{(!string.IsNullOrWhiteSpace(_ProjectTracking.RowKey) ? $" During the {_ProjectTracking.RowKey} build, Chad has had {_ProjectTracking.Oofs} {(_ProjectTracking.Oofs == 1 ? "oof" : "oofs")}." : string.Empty)}");
 		}
 
 		private void Stats()
 		{
-			twitchClient.SendMessage(Settings.ChannelName, $"Chad has dropped {_BricksDroped} {(_BricksDroped == 1 ? "brick" : "bricks")} and had {_Oofs} {(_Oofs == 1 ? "oof" : "oofs")} so far this stream.");
+			twitchClient.SendMessage(Settings.ChannelName, $"Chad has dropped {_BricksDropped} {(_BricksDropped == 1 ? "brick" : "bricks")} and had {_Oofs} {(_Oofs == 1 ? "oof" : "oofs")} so far this stream.");
 		}
 
+		private void ResetProject(OnChatCommandReceivedArgs commandArgs)
+		{
+			if (commandArgs.Command.ChatMessage.IsBroadcaster || commandArgs.Command.ChatMessage.IsModerator)
+			{
+				if (commandArgs.Command.ArgumentsAsList.Any())
+				{
+					_ProjectTracking = ProjectTracking.Retrieve(Settings.ChannelName, commandArgs.Command.ArgumentsAsString);
+					if (_ProjectTracking is null)
+					{
+						_ProjectTracking = new ProjectTracking(Settings.ChannelName, commandArgs.Command.ArgumentsAsString);
+						_ProjectTracking.Save();
+					}
+				}
+			}
+		}
+
+		private void UpdateProjectStats(int bricksDropped = 0, int oofs = 0)
+		{
+			_ProjectTracking.BricksDropped += bricksDropped;
+			_ProjectTracking.Oofs += oofs;
+		}
 
 	}
 
---
+++ src/TwitchBot/ProjectTracking.cs
@@ -0,0 +1,52 @@
+﻿using Azure;
+using Azure.Data.Tables;
+using System;
+using System.Linq;
+
+namespace TwitchBot
+{
+
+	public class ProjectTracking : ITableEntity
+	{
+		public string PartitionKey { get; set; }  // ChannelName
+		public string RowKey { get; set; } // ProjectName
+		public DateTimeOffset? Timestamp { get; set; }
+		public ETag ETag { get; set; }
+
+		public int BricksDropped { get; set; }
+		public int Oofs { get; set; }
+
+		public ProjectTracking() { }
+
+		public ProjectTracking(string channelName, string projectName)
+		{
+			PartitionKey = channelName;
+			RowKey = projectName;
+		}
+
+		private static TableClient GetTableClient()
+		{
+			TableClient tableClient;
+			tableClient = new TableClient(new Uri(Settings.StorageUri),
+					"BricksWithChad",
+					new TableSharedKeyCredential(Settings.StorageAccountName, Settings.StroageAccountKey));
+			return tableClient;
+		}
+
+		public static ProjectTracking Retrieve(string channelName, string projectName)
+		{
+
+			ProjectTracking projectTracking = GetTableClient().Query<ProjectTracking>(s => s.PartitionKey == channelName && s.RowKey == projectName).FirstOrDefault();
+
+			return projectTracking;
+
+		}
+
+		public void Save()
+		{
+			if (!string.IsNullOrWhiteSpace(PartitionKey) && !string.IsNullOrWhiteSpace(RowKey))
+				GetTableClient().UpsertEntity(this);
+		}
+
+	}
+}
